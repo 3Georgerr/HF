91b5a800854d5565a1d87e2add3ab9203027f6e3
Jiří Šeda <3.Georgerr@seznam.cz>
2017-02-15 19:30:36 +0100

    Initial

---
+++ LedFactory/HF.cpp
@@ -0,0 +1,140 @@
+/*  HF - High Flow WS2811 LED Library
+Copyright (c) 2017 Jiri seda
+
+*/
+
+#include "HF.h"
+
+
+void HF::blikej() {
+	//Hlídá interval pro zmìnu stavu
+	if (lastBlink + blinkDelay<millis()) {
+		change = true;
+		lastBlink = millis();
+	}
+	//Pokud se zmìnil stav, rozsvítí/zhasne pásek
+	if (change == true) {
+		change = false;
+		if (svit == true) {
+			for (uint16_t i = 0; i<strip.numPixels(); i++) {
+				strip.setPixelColor(i, blinkColorOne);
+			}
+		}
+		else {
+			for (uint16_t i = 0; i<strip.numPixels(); i++) {
+				strip.setPixelColor(i, blinkColorTwo);
+			}
+		}
+		//strip.show();
+		svit = !svit;
+	}
+}
+
+void HF::startuj() {
+	if (startTime + startDelay < millis()) {
+		change = true;
+	}
+
+	if (change == true) {
+		change = false;
+		startTime = millis();
+
+		switch (startPhase) {
+		case 0:
+			for (uint16_t i = 0; i < numPix*0.33; i++) {
+				strip.setPixelColor(i, startColorOne);
+			}
+			for (uint16_t i = numPix*0.33; i < numPix; i++) {
+				strip.setPixelColor(i, strip.Color(0, 0, 0));
+			}
+			//strip.show();
+			break;
+		case 1:
+			for (uint16_t i = 0; i < numPix*0.66; i++) {
+				strip.setPixelColor(i, startColorTwo);
+			}
+			for (uint16_t i = numPix*0.66; i < numPix; i++) {
+				strip.setPixelColor(i, strip.Color(0, 0, 0));
+			}
+			//strip.show();
+		case 2:
+			for (uint16_t i = 0; i < numPix; i++) {
+				strip.setPixelColor(i, startColorThree);
+			}
+			//strip.show();
+			break;
+		default:
+		//Po odstartování vypne pásek;
+			setMode(0);
+			break;
+		}
+		startPhase++;
+	}
+}
+
+void HF::vypnout()
+{
+	if (changedMode || (millis() % REFRESH_RATE) ) {
+		for (uint16_t i = 0; i < numPix; i++) {
+			strip.setPixelColor(i, strip.Color(0, 0, 0));
+		}
+		//strip.show();
+	}
+}
+
+//mode 0 = vypnuto
+//mode 1 = blinking
+//mode 2 = starting
+//mode 3 = light
+void HF::loop() {
+	switch (mode) {
+	case 0:
+		vypnout();
+		break;
+	case 1:
+		blikej();
+		break;
+	case 2:
+		startuj();
+		break;
+	case 3:
+		light();
+		break;
+	default:
+		break;
+	}
+	changedMode = false;
+}
+
+void HF::light()
+{
+	//Obnovuje nastavení svìtel, pokud se zmìní stav nebo uteklo 1000 sekund 
+	if (changedMode || (millis() % REFRESH_RATE) ) {
+		for (uint16_t i = 0; i < numPix; i++) {
+			strip.setPixelColor(i, confColor);
+		}
+		//strip.show();
+	}
+}
+
+void HF::setMode(uint8_t mode)
+{
+	this->mode = mode;
+	changedMode = true;
+
+	if (mode == 1) {
+		change = true;
+		svit = true;
+
+	}
+}
+
+void HF::setColor(uint8_t r, uint8_t g, uint8_t b)
+{
+	confColor = ((uint32_t)r << 16) | ((uint32_t)g << 8) | b;
+}
+
+void HF::showIt()
+{
+	strip.show();
+}
--- LedFactory/LedFactory.ino
+++ LedFactory/LedFactory.ino
@@ -1,13 +1,64 @@
-void setup()
-{
+#pragma once 
+#pragma GCC optimize ("O0")
 
-  /* add setup code here */
+#include "HF.h"
+//#include "NetworkCommunication.h"
+#include <Adafruit_NeoPixel.h>
 
+HF prvni = HF(1000, 6, NEO_GRB + NEO_KHZ800);
+HF druhy = HF(1000, 2, NEO_GRB + NEO_KHZ800);
+HF treti = HF(1060, 1, NEO_GRB + NEO_KHZ800);
+HF ctvrty = HF(1060, 3, NEO_GRB + NEO_KHZ800);
+HF paty = HF(1060, 4, NEO_GRB + NEO_KHZ800);
+HF sesty = HF(1060, 5, NEO_GRB + NEO_KHZ800);
+uint32_t counter = 0;
+
+uint8_t mac[6] = { 0x00,0x01,0x02,0x03,0x04,0x05 };
+//IPAddress ip = IPAddress(10, 0, 0, 34);
+//NetworkCommunication nc = NetworkCommunication(ip, mac);
+
+void setup() {
+	// put your setup code here, to run once:
+	delay(10);
+	prvni = HF(1000, 6, NEO_GRB + NEO_KHZ800);
+	druhy = HF(1000, 2, NEO_GRB + NEO_KHZ800);
+	treti = HF(1060, 1, NEO_GRB + NEO_KHZ800);
+	ctvrty = HF(1060, 3, NEO_GRB + NEO_KHZ800);
+	paty = HF(1060, 4, NEO_GRB + NEO_KHZ800);
+	sesty = HF(1060, 5, NEO_GRB + NEO_KHZ800);
 }
 
-void loop()
-{
+void loop() {
+	// put your main code here, to run repeatedly:
 
-  /* add main program code here */
+	if (millis() % 100 == 1) {
+		druhy.loop();
+		prvni.loop();
+		treti.loop();
+		ctvrty.loop();
+		paty.loop();
+		sesty.loop();
+	}
+	//delay(10);
+	//Vykresli pásky, zakaž pøerušení
+	//noInterrupts();
+	//if (millis() % 100 == 1) {
+	druhy.showIt();
+	prvni.showIt();
+	treti.showIt();
+	ctvrty.showIt();
+	paty.showIt();
+	sesty.showIt();
+	//	}
+	//interrupts();
+
+	if (millis()>20000) {
+		prvni.setMode(3);
+		//delay(1);
+		//druhy.setMode(3);
+	}
+
+
+	//nc.loop();
 
 }
