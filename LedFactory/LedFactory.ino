57addba603047ae2363014ef203fcc446c035d4f
Jiří Šeda <3.Georgerr@seznam.cz>
2017-02-21 20:41:59 +0100

    Nedynamicky

--- LedFactory/LedFactory.ino
+++ LedFactory/LedFactory.ino
@@ -7,19 +7,13 @@
 #include <Adafruit_NeoPixel.h>
 #include "LedController.h"
 
-//HF prvni = HF(60, 6, NEO_GRB + NEO_KHZ800);
-//HF druhy = HF(90, 2, NEO_GRB + NEO_KHZ800);
-//HF treti = HF(240, 1, NEO_GRB + NEO_KHZ800);
-//HF ctvrty = HF(240, 3, NEO_GRB + NEO_KHZ800);
-//HF paty = HF(240, 4, NEO_GRB + NEO_KHZ800);
-//HF sesty = HF(240, 5, NEO_GRB + NEO_KHZ800);
+HF prvni = HF(60, 6, NEO_GRB + NEO_KHZ800);
+HF druhy = HF(90, 2, NEO_GRB + NEO_KHZ800);
+HF treti = HF(240, 1, NEO_GRB + NEO_KHZ800);
+HF ctvrty = HF(240, 3, NEO_GRB + NEO_KHZ800);
+HF paty = HF(240, 4, NEO_GRB + NEO_KHZ800);
+HF sesty = HF(240, 5, NEO_GRB + NEO_KHZ800);
 uint32_t counter = 0;
-HF *prvni;
-HF *druhy;
-HF *treti;
-HF *ctvrty;
-HF *paty;
-HF *sesty;
 
 uint8_t mac[6] = { 0x00,0x01,0x02,0x03,0x04,0x05 };
 IPAddress ip = IPAddress(10, 0, 0, 34);
@@ -39,20 +33,14 @@ void setup() {
 	//ip = IPAddress(10, 0, 0, 34);
 	//nc = NetworkCommunication(mac, ip, 6);
 	*/
-	prvni=new HF(60, 6, NEO_GRB + NEO_KHZ800);
-	druhy = new HF(90, 6, NEO_GRB + NEO_KHZ800);
-	treti = new HF(240, 6, NEO_GRB + NEO_KHZ800);
-	ctvrty = new HF(240, 6, NEO_GRB + NEO_KHZ800);
-	paty = new HF(240, 6, NEO_GRB + NEO_KHZ800);
-	sesty = new HF(240, 6, NEO_GRB + NEO_KHZ800);
+
+	lc.setStrip(&prvni, 0);
 	
-	lc.setStrip(prvni, 0);
-	
-	lc.setStrip(druhy, 1);
-	lc.setStrip(treti, 2);
-	lc.setStrip(ctvrty, 3);
-	lc.setStrip(paty, 4);
-	lc.setStrip(sesty, 5);
+	lc.setStrip(&druhy, 1);
+	lc.setStrip(&treti, 2);
+	lc.setStrip(&ctvrty, 3);
+	lc.setStrip(&paty, 4);
+	lc.setStrip(&sesty, 5);
 	
 	//lc.getStrip(0)->blinkColor(0, 10, 1, 0, 1, 1);
 	lc.getStrip(0)->blinkColor(0, 0, 1, 0, 1, 0);
@@ -63,39 +51,39 @@ void setup() {
 	//prvni.blinkColor(0, 0, 255);
 	//druhy.blinkColor(1, 0, 0, 0, 1, 0);
 	//prvni.blinkColor(0, 0, 255, 0, 255, 0);
-	
+
 	nc.start();
 }
 
 void loop() {
 	
 	if (millis() % 100 == 1) {
-		druhy->loop();
-		prvni->loop();
-		treti->loop();
-		ctvrty->loop();
-		paty->loop();
-		sesty->loop();
+		druhy.loop();
+		prvni.loop();
+		treti.loop();
+		ctvrty.loop();
+		paty.loop();
+		sesty.loop();
 	}
 	//delay(10);
 	//Vykresli pásky, zakaž pøerušení
 	noInterrupts();
 	if (millis() % 100 == 1) {
-		druhy->showIt();
-		prvni->showIt();
-		treti->showIt();
-		ctvrty->showIt();
-		paty->showIt();
-		sesty->showIt();
+		druhy.showIt();
+		prvni.showIt();
+		treti.showIt();
+		ctvrty.showIt();
+		paty.showIt();
+		sesty.showIt();
 	}
 	interrupts();
 
 	if (millis()>5000 && counter ==0) {
-		prvni->setMode(3);
-		lc.getStrip(0)->setMode(3); //nefunguje
-		//prvni->setColor(255,255,0); //nefunguje
-		//lc.getStrip(0)->setColor(255, 0, 0);
-		druhy->setMode(3);
+		prvni.setMode(3);
+		//lc.getStrip(0)->setMode(3); //nefunguje
+		//prvni.setColor(255,0,0); //nefunguje
+		lc.getStrip(0)->setColor(255, 0, 0);
+		druhy.setMode(3);
 		//lc.getStrip(1)->setColor(0, 255, 0);
 		counter++;
 		//delay(1);
